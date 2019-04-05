a64f0d608d2decd9dc9497070ce96b7245d818ac
YB1010 <37540709+YB1010@users.noreply.github.com>
2019-04-06 00:40:34 +1100

    test

--- PP1/web/js/map.js
+++ PP1/web/js/map.js
@@ -60,7 +60,7 @@ function showCarsOnMap(map,carlist,image) {
     }
 
 }
-
+ 
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: -34.397, lng: 150.644},
