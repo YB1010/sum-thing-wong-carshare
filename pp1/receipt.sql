86f65b5760d47f883d73cab785322d02b75420ae
MichaelYao1998 <32200218+MichaelYao1998@users.noreply.github.com>
2019-04-23 12:51:04 +1000

    Delete receipt.sql

--- pp1/receipt.sql
+++
@@ -1,7 +0,0 @@
-create table `receipt`(
-  `email` varchar(30) not null,
-  `car_id` int(11) not null,
-  `start_date` TIMESTAMP not null,
-  `balance` int(11) not null,
-   PRIMARY KEY(email,car_id,start_date)
-);
