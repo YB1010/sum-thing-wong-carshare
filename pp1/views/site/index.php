cdf7b3d10782581873275e88015be91f49699208
rmit-s3549962-yangming-an <s3549962@student.rmit.edu.au>
2019-05-14 00:26:19 +0800

    Add files via upload
    commit

--- pp1/views/site/index.php
+++ pp1/views/site/index.php
@@ -4,7 +4,7 @@
 
 $this->title = 'My Yii Application';
 ?>
-<body style="background: url('img/bg.jpg');background-size：100% 100%;background-attachment: fixed;background-repeat:no-repeat;text-align: center;width: 100%;height: 100%">
+<body style="background: url('img/bg.jpg');background-size:100%;background-attachment: fixed;background-repeat:no-repeat;text-align: center;width: 100%;height: 100%">
 <div class="site-index" >
 
     <div class="jumbotron" style="width: 100%;text-align: center">
