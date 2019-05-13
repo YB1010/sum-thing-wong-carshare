bf047b48aab2aea9b92566688568a4c7e83a38cd
rmit-s3549962-yangming-an <s3549962@student.rmit.edu.au>
2019-05-13 23:48:43 +0800

    Add files via upload
    commit

--- pp1/views/site/index.php
+++ pp1/views/site/index.php
@@ -4,20 +4,20 @@
 
 $this->title = 'My Yii Application';
 ?>
-<div class="site-index">
+<div class="site-index" style="background: url('img/bg.jpg');text-align: center">
 
     <div class="jumbotron">
-        <h1>Sum-thing-wrong's SOCAR</h1>
-        <p class="lead"></p>
+        <h1><font color="white">Sum-thing-wrong's SOCAR</font></h1></h1>
+        <p ></p>
     </div>
 
-    <div class="body-content" style="background: url('img/bg.jpg') ">
+    <div class="body-content"  style="text-align: center">
 
         <div class="row">
             <div class="col-lg-4" s>
-                <h1>Enjoy the best CAR Renting Experience</h1>
-                <p> </p>
-                <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com"><h1>EVER</h1></a></p>
+                <h1><font color="white">Enjoy the best CAR Renting Experience</font></h1>
+
+                <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com" style="width: 150px"><h1>EVER</h1></a></p>
             </div>
 
         </div>
