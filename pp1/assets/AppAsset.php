64839811c0bbfeb70f3ab30c489afe1fa24791b2
rmit-s3549962-yangming-an <s3549962@student.rmit.edu.au>
2019-05-16 00:22:52 +0800

    Add files via upload

--- pp1/assets/AppAsset.php
+++ pp1/assets/AppAsset.php
@@ -21,7 +21,7 @@ class AppAsset extends AssetBundle
     public $baseUrl = '@web';
     public $css = [
         'css/site.css',
-        'bootstrap.min.css'
+        'css/bootstrap.min.css'
     ];
     public $js = [
         'js/bootstrap.min.js',
