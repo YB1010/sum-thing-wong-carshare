eaf2a8e8ade980d9fc940089faffaa85529a46ff
rmit-s3549962-yangming-an <s3549962@student.rmit.edu.au>
2019-05-15 11:58:46 +0800

    Add files via upload

--- pp1/views/site/index.php
+++ pp1/views/site/index.php
@@ -4,50 +4,23 @@
 
 $this->title = 'My Yii Application';
 ?>
-<div class="site-index">
+<body style="background: url('img/bg.jpg');background-size:100%;background-attachment: fixed;background-repeat:no-repeat;text-align: center;width: 100%;height: 100%">
+<div class="site-index" >
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
+    <div class="jumbotron" style="width: 100%;text-align: center">
+        <h1><font color="white">Sum-thing-wrong's SOCAR</font></h1></h1>
     </div>
 
-    <div class="body-content">
+    <div class="body-content"  style="text-align: center">
 
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
+        <div class="row" style="text-align: center;width: 100%">
+            <div class="col-lg-4" style="text-align: center;width: 100%">
+                <h1><font color="white">Enjoy the best CAR Renting Experience</font></h1>
+                <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com" style="width: 250px">EVER</a></p>
             </div>
         </div>
 
     </div>
 </div>
+</body>
+
