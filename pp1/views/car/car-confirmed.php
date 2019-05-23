a18df285647cca19500ea12e45d2b4320608d69f
rmit-s3549962-yangming-an <s3549962@student.rmit.edu.au>
2019-05-23 21:07:16 +0800

    Add files via upload

--- pp1/views/car/car-confirmed.php
+++ pp1/views/car/car-confirmed.php
@@ -1,15 +1,12 @@
 <?php
-
 /**
  * Created by PhpStorm.
  * User: Yuchen Yao
  * Date: 11/04/2019
  * Time: 7:44 PM
  */
-
 use app\assets\PaypalCallback;
 use yii\helpers\Html;
-
 /* @var $this \yii\web\View */
 \app\assets\AppAsset::register($this);
 PaypalCallback::register($this);
@@ -19,8 +16,8 @@ PaypalCallback::register($this);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 </head>
-<body onload="pendingTime()">
-
-<span id="count"></span>
-<div id="paypal-button-container"></div> <h1>To Confirm</h1>
+<body onload="pendingTime()" style="text-align:center;">
+<h1>To Confirm</h1>
+<div id="paypal-button-container"></div> 
+<h2><span id="count" ></span></h2>
 </body>
